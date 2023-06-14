f925113137cf39bbd18a9d651deb93ec03d0dc69
mahendra uppalapati <mahendra.u@fineantconsultantservices.com>
2023-06-14 15:08:11 +0530

    reacticons changed

--- src/pages/Dropdownprofiles.js
+++ src/pages/Dropdownprofiles.js
@@ -1,20 +1,20 @@
-import React from "react"
-import style from "./../../src/styles/Homepage.module.css";
-import {HiOutlineLogout} from "react-icons/hi"
-function Dropdownprofiles(){
- return(
-    <div className={style.dropdownprofiles}>
-        <ul>
-         <li>My Profile</li>
-         <li>My Portfolio</li>
-         <li>Equipments and Gear</li>
-         <li>Previous Bookings</li>
-         <hr/>
-         <li className={style.icon}><HiOutlineLogout/>Logout</li>
+// import React from "react"
+// import style from "./../../src/styles/Homepage.module.css";
+// import {HiOutlineLogout} from "react-icons/hi"
+// function Dropdownprofiles(){
+//  return(
+//     <div className={style.dropdownprofiles}>
+//         <ul>
+//          <li>My Profile</li>
+//          <li>My Portfolio</li>
+//          <li>Equipments and Gear</li>
+//          <li>Previous Bookings</li>
+//          <hr/>
+//          <li className={style.icon}><HiOutlineLogout/>Logout</li>
 
-        </ul>
+//         </ul>
 
-    </div>
- )
-}
-export default Dropdownprofiles;
+//     </div>
+//  )
+// }
+// export default Dropdownprofiles;
