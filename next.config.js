b93804a90d9a30a7854fabbfba23f212f5d66f8f
Shravan <shravan.sonnaila@fineantconsultantservices.com>
2023-06-16 09:04:15 +0000

    next.config.js edited online with Bitbucket

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  server: {
+    port: 80, // Specify the desired port number here
+  }, 
 }
 
 module.exports = nextConfig
